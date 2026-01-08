52bba5e9b6b499f58c48903785f69bc7531e265d
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2026-01-08 11:23:44 -0300

    enabling cors for fronted requests

--- src/main/java/com/pedroacbg/catalog/controller/CategoryController.java
+++ src/main/java/com/pedroacbg/catalog/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.pedroacbg.catalog.controller;
 import com.pedroacbg.catalog.model.Category;
 import com.pedroacbg.catalog.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CategoryController {
 
     @Autowired
--- src/main/java/com/pedroacbg/catalog/controller/ProductController.java
+++ src/main/java/com/pedroacbg/catalog/controller/ProductController.java
@@ -3,15 +3,13 @@ package com.pedroacbg.catalog.controller;
 import com.pedroacbg.catalog.model.Product;
 import com.pedroacbg.catalog.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
 
     @Autowired
